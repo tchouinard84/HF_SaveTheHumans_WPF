48a655aaf217b57476a430ce6cd523a0590635ca
tchouina <timothy.chouinard@emp-corp.com>
2017-09-29 15:33:17 -0400

    Made the game better

--- SaveTheHumans/SaveTheHumans/MainWindow.xaml.cs
+++ SaveTheHumans/SaveTheHumans/MainWindow.xaml.cs
@@ -22,30 +22,30 @@ namespace SaveTheHumans
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Random random = new Random();
-        private DispatcherTimer enemyTimer = new DispatcherTimer();
-        private DispatcherTimer targetTimer = new DispatcherTimer();
-        private bool humanCaptured = false;
+        private const int SENSITIVITY = 3;
+        private const double ADD_ENEMY_INTERVAL = 2;
+        private const double PROGRESS_BAR_INTERVAL = .1;
+
+        private readonly Random _random = new Random();
+        private readonly DispatcherTimer _enemyTimer = new DispatcherTimer();
+        private readonly DispatcherTimer _targetTimer = new DispatcherTimer();
+        private bool _humanCaptured = false;
+        private int _humansSaved;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            enemyTimer.Tick += enemyTimer_Tick;
-            enemyTimer.Interval = TimeSpan.FromSeconds(2);
+            _enemyTimer.Tick += enemyTimer_Tick;
+            _enemyTimer.Interval = TimeSpan.FromSeconds(ADD_ENEMY_INTERVAL);
 
-            targetTimer.Tick += targetTimer_Tick;
-            targetTimer.Interval = TimeSpan.FromSeconds(.1);
+            _targetTimer.Tick += targetTimer_Tick;
+            _targetTimer.Interval = TimeSpan.FromSeconds(PROGRESS_BAR_INTERVAL);
         }
 
         private void targetTimer_Tick(object sender, EventArgs e)
         {
             progressBar.Value += 1;
-            MaybeEndTheGame();
-        }
-
-        private void MaybeEndTheGame()
-        {
             if (progressBar.Value < progressBar.Maximum) { return; }
             if (playArea.Children.Contains(gameOverText)) { return; }
             EndTheGame();
@@ -53,11 +53,14 @@ namespace SaveTheHumans
 
         private void EndTheGame()
         {
-            enemyTimer.Stop();
-            targetTimer.Stop();
-            humanCaptured = false;
+            _enemyTimer.Stop();
+            _targetTimer.Stop();
+            _humanCaptured = false;
             startButton.Visibility = Visibility.Visible;
-            playArea.Children.Add(gameOverText);
+            humanCountMsg.Visibility = Visibility.Hidden;
+            gameEndStatusText.Text = "You saved " + _humansSaved + " humans!";
+            gameEnd.Visibility = Visibility.Visible;
+            playArea.Children.Add(gameEnd);
         }
 
         private void enemyTimer_Tick(object sender, EventArgs e)
@@ -72,25 +75,35 @@ namespace SaveTheHumans
 
         private void StartGame()
         {
+            _humansSaved = 0;
             human.IsHitTestVisible = true;
-            humanCaptured = false;
+            _humanCaptured = false;
             progressBar.Value = 0;
             startButton.Visibility = Visibility.Collapsed;
+            currentLevel.Text = _humansSaved.ToString();
+            humanCountMsg.Visibility = Visibility.Visible;
             playArea.Children.Clear();
             playArea.Children.Add(target);
             playArea.Children.Add(human);
-            enemyTimer.Start();
-            targetTimer.Start();
+            _enemyTimer.Start();
+            _targetTimer.Start();
         }
 
         private void AddEnemy()
         {
-            var enemy = new ContentControl();
-            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            var enemy = new ContentControl { Template = Resources["EnemyTemplate"] as ControlTemplate };
             AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
-            AnimateEnemy(enemy, random.Next((int) playArea.ActualHeight - 100),
-                random.Next((int) playArea.ActualHeight - 100), "(Canvas.Top)");
+            AnimateEnemy(enemy, _random.Next((int) playArea.ActualHeight - 100),
+                _random.Next((int) playArea.ActualHeight - 100), "(Canvas.Top)");
             playArea.Children.Add(enemy);
+
+            enemy.MouseEnter += enemy_MouseEnter;
+        }
+
+        private void enemy_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (_humanCaptured)
+                EndTheGame();
         }
 
         private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
@@ -100,7 +113,7 @@ namespace SaveTheHumans
             {
                 From = from,
                 To = to,
-                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+                Duration = new Duration(TimeSpan.FromSeconds(_random.Next(4, 6)))
             };
             Storyboard.SetTarget(animation, enemy);
             Storyboard.SetTargetProperty(animation, new PropertyPath(propertyToAnimate));
@@ -110,9 +123,54 @@ namespace SaveTheHumans
 
         private void human_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (!enemyTimer.IsEnabled) { return; }
-            humanCaptured = true;
+            if (!_enemyTimer.IsEnabled) { return; }
+            _humanCaptured = true;
             human.IsHitTestVisible = false;
+        }
+
+        private void target_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (!_targetTimer.IsEnabled) { return; }
+            if (!_humanCaptured) { return; }
+            NextHuman();
+        }
+
+        private void NextHuman()
+        {
+            progressBar.Value = 0;
+            Canvas.SetLeft(target, _random.Next(100, (int) playArea.ActualWidth - 100));
+            Canvas.SetTop(target, _random.Next(100, (int) playArea.ActualHeight - 100));
+            Canvas.SetLeft(human, _random.Next(100, (int) playArea.ActualWidth - 100));
+            Canvas.SetTop(human, _random.Next(100, (int) playArea.ActualHeight - 100));
+            _humanCaptured = false;
+            human.IsHitTestVisible = true;
+            _humansSaved++;
+            currentLevel.Text = _humansSaved.ToString();
+        }
+
+        private void playArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_humanCaptured) { return; }
+            var pointerPosition = e.GetPosition(null);
+            var relativePosition = grid.TransformToVisual(playArea).Transform(pointerPosition);
+            if ((Math.Abs(relativePosition.X - Canvas.GetLeft(human)) > human.ActualWidth * SENSITIVITY)
+                || (Math.Abs(relativePosition.Y - Canvas.GetTop(human)) > human.ActualHeight * SENSITIVITY))
+            {
+                _humanCaptured = false;
+                human.IsHitTestVisible = true;
+            }
+            else
+            {
+                Canvas.SetLeft(human, relativePosition.X - human.ActualWidth / 2);
+                Canvas.SetTop(human, relativePosition.Y - human.ActualHeight / 2);
+
+            }
+        }
+
+        private void playArea_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (_humanCaptured)
+                EndTheGame();
         }
     }
 }
